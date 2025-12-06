feb60b91d9ec5521d33791c650bc06e6d0431392
Ryui-code <ulanbektalasbaev2020@gmail.com>
2025-12-06 20:43:24 +0600

    Изменения

--- settings/glovo/permissions.py
+++ settings/glovo/permissions.py
@@ -6,27 +6,27 @@ class StatusBasedPermission(BasePermission):
         if not user.is_authenticated:
             return False
 
-        role = getattr(user, 'status', None)
+        status = getattr(user, 'status', None)
         try:
             model_name = view.get_queryset().model.__name__
         except Exception:
             return True
 
-        if role == 'Seller':
+        if status == 'Seller':
             if model_name == 'Store':
                 return True
             if model_name == 'StoreRating':
                 return request.method in SAFE_METHODS
             return False
 
-        if role == 'Client':
+        if status == 'Client':
             if model_name == 'Store':
                 return request.method in SAFE_METHODS
             if model_name in ['Cart', 'Order', 'CourierRating', 'StoreRating']:
                 return True
             return False
 
-        if role == 'Courier':
+        if status == 'Courier':
             if model_name in ['Order', 'CourierRating']:
                 return request.method in SAFE_METHODS
             return False
